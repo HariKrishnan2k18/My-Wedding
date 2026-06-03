55cce13b34d05488d144bffae6ae8e99975fa769
Jithin Narayanan <jithin.narayanan@luladelivery.com>
2026-06-03 16:13:11 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,30 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Hari 🫶🏻 Dhana",
   description:
-    "With the blessings of the Almighty, we joyfully invite you and your family to the wedding of Hari & Dhana on 24th June 2026 at at Srinivasa Mandapam, Udayarpalayam, Thammampatti.",
+    "With the blessings of the Almighty, we joyfully invite you and your family to the wedding of Hari & Dhana on 24th June 2026 at Srinivasa Mandapam, Udayarpalayam, Thammampatti.",
   authors: [{ name: "Hari & Dhana" }],
   openGraph: {
     type: "website",
+    url: "https://harikrishnan-weds-dhana.netlify.app/",
     title: "Wedding Invitation | Hari & Dhana | 24th June 2026",
     description:
       "Join us to celebrate the wedding of Hari & Dhana on 24th June 2026 at Srinivasa Mandapam, Udayarpalayam, Thammampatti.",
     siteName: "Wedding Invitation | Hari & Dhana",
+    images: [
+      {
+        url: "https://harikrishnan-weds-dhana.netlify.app/assets/Invitation%20Card.png",
+        width: 1200,
+        height: 630,
+        alt: "Hari & Dhana Wedding Invitation – 24th June 2026",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Wedding Invitation | Hari & Dhana | 24th June 2026",
+    description:
+      "Join us to celebrate the wedding of Hari & Dhana on 24th June 2026 at Srinivasa Mandapam, Udayarpalayam, Thammampatti.",
+    images: ["https://harikrishnan-weds-dhana.netlify.app/assets/Invitation%20Card.png"],
   },
 };
 
